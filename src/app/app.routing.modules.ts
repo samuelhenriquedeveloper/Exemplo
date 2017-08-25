67a52858f0638fdea3884768a48b6a341b97e2d8
Samuel Henrique de Oliveira Araújo <samuelhenrique@gmail.com>
2017-08-24 22:56:24 -0300

    Estrutura inicial

---
+++ src/app/app.routing.modules.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent  },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', component: PaginaNaoEncontradaComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes, {useHash: false})],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
